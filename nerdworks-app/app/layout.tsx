fe01e2e1d1dca9dfa9eef74542f1881e6dd73cc1
georgewillpraise15 <georgewillpraise15@gmail.com>
2025-05-07 03:32:26 +0000

    added the font folder

--- nerdworks-app/app/layout.tsx
+++ nerdworks-app/app/layout.tsx
@@ -1,17 +1,13 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import { ThemeProvider } from "next-themes";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const inter = Inter({
+  variable: "--font-inter",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "NerdWorks",
